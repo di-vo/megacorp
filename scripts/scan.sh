ab95d2088f1041a8f8b246104209d846fedca807
di-vo <jan.reuter2909@gmail.com>
2025-03-07 17:30:04 +0100

    P: Revert scan.sh
    This reverts commit e93454ec2a35c9d8308b59cee578465c292699e7.

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,12 +1,1 @@
-echo "\n===== SCANNING FOR CREDIT CARD NUMBERS ====="
-grep -rE --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git} --line-number
-echo "\n===== CREDIT CARD SCAN COMPLETE ====="
-
-echo "\n===== SCANNING FOR SOCIAL SECURIY NUMBERS ====="
-grep -rE --color=always '\b[0-9]{3}-[0-9]{2}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
-echo "\n===== SOCIAL SECURIY SCAN COMPLETE ====="
-
-echo "\n===== SCANNING FOR PHONE NUMBERS ====="
-grep -rE --color=always '\b[0-9]{3}-[0-9]{3}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
-grep -rE --color=always '\([0-9]{3}) [0-9]{3}-[0-9]{4}' . --exclude-dir={.git} --line-number
-echo "\n===== PHONE NUMBER SCAN COMPLETE ====="
+# TODO: write the script
